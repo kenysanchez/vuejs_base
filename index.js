778fd6bbcbd5be3c1ccd77e46e12808dfe71582e
Kenia Sánchez <kenya.gaby@hotmail.com>
2022-03-15 20:11:38 -0600

    ACT EN CLASE

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hola!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        bucl:[
+            { message:"Cacatua Ninfa"  },
+            { message:"Cantan a las 7 de la manana" },
+            { message:'Te dicen FIUUUU FIUUUU' },
+        ]
+    }
+  })
