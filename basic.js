f47088951d7482d0c8669c5866a0d1235f02b1e4
Kenia Sánchez <kenya.gaby@hotmail.com>
2022-03-15 20:11:12 -0600

    .

--- basic.js
+++ basic.js
@@ -4,3 +4,14 @@ var app = new Vue({
         message: "Hello Vue"
     }
 });
+
+var app2 = new Vue({
+    el:'#app-2',
+    data:{
+        bucl : [
+        { message: "primer pagina ok"},
+        { message: "segunda pagina nok"},
+        { message: "segunda pagina nok"},
+        ] 
+    }
+});
